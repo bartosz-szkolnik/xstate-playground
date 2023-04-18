726cb27400ab24135c629b06068c526563135800
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2023-04-18 23:30:34 +0200

    Remove magic & little fixes

--- src/main.ts
+++ src/main.ts
@@ -31,25 +31,21 @@ const dragDropMachine = createMachine({
       on: {
         mousemove: {
           target: 'dragging',
-          actions: assign((context, event: MouseEvent) => {
-            return {
-              ...context,
-              dx: event.clientX - context.pointerx,
-              dy: event.clientY - context.pointery,
-            };
-          }),
+          actions: assign((context, event: MouseEvent) => ({
+            ...context,
+            dx: event.clientX - context.pointerx,
+            dy: event.clientY - context.pointery,
+          })),
         },
         mouseup: {
           target: 'idle',
-          actions: assign(context => {
-            return {
-              ...context,
-              x: context.x + context.dx,
-              y: context.y + context.dy,
-              dx: 0,
-              dy: 0,
-            };
-          }),
+          actions: assign(context => ({
+            ...context,
+            x: context.x + context.dx,
+            y: context.y + context.dy,
+            dx: 0,
+            dy: 0,
+          })),
         },
       },
     },
@@ -62,7 +58,6 @@ const box = document.getElementById('box');
 const dragDropService = interpret(dragDropMachine)
   .onTransition(state => {
     if (state.changed) {
-      console.log(state.context);
       box?.style.setProperty('left', `${state.context.x + state.context.dx}px`);
       box?.style.setProperty('top', `${state.context.y + state.context.dy}px`);
       body.dataset.state = state.toStrings().join(' ');
@@ -71,13 +66,17 @@ const dragDropService = interpret(dragDropMachine)
   .start();
 
 box?.addEventListener('mousedown', event => {
-  dragDropService.send(event);
+  dragDropService.send({ type: 'mousedown', ...toEvent(event) });
 });
 
 body.addEventListener('mouseup', () => {
-  dragDropService.send('mouseup');
+  dragDropService.send({ type: 'mouseup' });
 });
 
 body.addEventListener('mousemove', event => {
-  dragDropService.send(event);
+  dragDropService.send({ type: 'mousemove', ...toEvent(event) });
 });
+
+function toEvent({ clientX, clientY }: MouseEvent) {
+  return { clientX, clientY };
+}
